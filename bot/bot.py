b98e337ed98d736ba638e994e64e505e77622bdc
andyjjrt <andyjjrt@gmail.com>
2025-03-12 01:21:42 +0800

    fix: Change activity

--- bot/bot.py
+++ bot/bot.py
@@ -24,8 +24,8 @@ logger = logging.getLogger('uvicorn.error')
 @bot.event
 async def on_ready():
     logger.info(f"We have logged in as {bot.user}")
-    game = discord.Activity(type=discord.ActivityType.competing, name="Testing")
-    await bot.change_presence(status=discord.Status.do_not_disturb, activity=game)
+    status = discord.Activity(type=discord.ActivityType.listening, name="/play")
+    await bot.change_presence(status=discord.Status.online, activity=status)
 
 
 @bot.event
